5e66a25fa39d69247a3a6ccdcc787edc074ef8c3
louisveLiu <louisve8099@gmail.com>
2023-05-14 01:14:17 +0100

    y

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,4 +1,5 @@
 from numbers import Number
+from numbers import Integral
 
 
 class Polynomial:
@@ -50,3 +51,50 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+
+    def __sub__(self, other):
+        if isinstance(other, Polynomial):
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a - b for a, b in zip(self.coefficients,
+                                                other.coefficients))
+            coefs += (self.coefficients[common:] +
+                      tuple(-1 * a for a in other.coefficients[common:]))
+
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] + (-1) * other,)
+                              + self.coefficients[1:])
+
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        return other + Polynomial(tuple(-1 * b for b in self.coefficients[0:]))
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            result_coeff = [0 for n in range(self.degree()+other.degree()+1)]
+            for i in range(len(self.coefficients)):
+                for j in range(len(other.coefficients)):
+                    result_coeff[i+j] += (list(self.coefficients)[i] *
+                                          list(other.coefficients)[j])
+            return Polynomial(tuple(result_coeff))
+
+        elif isinstance(other, Number):
+            return Polynomial(tuple(other * a for
+                                    a in list(self.coefficients)[0:]))
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __pow__(self, other):
+
+        if isinstance(other, Integral):
+            power = self
+            for i in range(other-1):
+                power = self * power
+            return power
